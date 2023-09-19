1f4019a7b5e8bd351ed3759d7b551846d195f559
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-19 14:24:30 +0300

    reorganize to one big parametrize with ids

--- tests/level_2/test_five_replace_word.py
+++ tests/level_2/test_five_replace_word.py
@@ -6,27 +6,30 @@ from functions.level_2.five_replace_word import replace_word
 @pytest.mark.parametrize(
     'text, expected',
     [
-        ('welcome to the jungle', 'welcome to the forest'),
-        (
+        pytest.param(
+            'welcome to the jungle',
+            'welcome to the forest',
+            id='replace_one_word',
+        ),
+        pytest.param(
             'welcome to the jungle we got your disease in the jungle',
-            'welcome to the forest we got your disease in the forest'
+            'welcome to the forest we got your disease in the forest',
+            id='replace_two_words',
+        ),
+        pytest.param(
+            'welcome to the jungle. we got your disease in the jungle',
+            'welcome to the jungle. we got your disease in the forest',
+            id='doesnt_work_with_punctuation'
+        ),
+        pytest.param(
+            'Welcome to the JunGle we got your disease in the jungle',
+            'Welcome to the forest we got your disease in the forest',
+            id='independent_from_letter_case'
         ),
     ]
 )
-def test__replace_word__replace_one_or_more_word(text, replace_from, replace_to, expected):
+def test__replace_word(text, replace_from, replace_to, expected):
     assert replace_word(text=text, replace_from=replace_from, replace_to=replace_to) == expected
-
-
-def test__replace_word__doesnt_work_with_punctuation(replace_from, replace_to):
-    text = 'welcome to the jungle. we got your disease in the jungle'
-    actual_result = replace_word(text=text, replace_from=replace_from, replace_to=replace_to)
-    assert actual_result == 'welcome to the jungle. we got your disease in the forest'
-
-
-def test__replace_word__independent_from_letter_case(replace_from, replace_to):
-    text = 'Welcome to the JunGle we got your disease in the jungle'
-    actual_result = replace_word(text=text, replace_from=replace_from, replace_to=replace_to)
-    assert actual_result == 'Welcome to the forest we got your disease in the forest'
 
 
 def test__replace_word__doesnt_replace_gaps():
