757f7d6722a4fdb09cdbe4d377087665a6b7b23c
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-19 14:23:14 +0300

    add ids to parametrize cases

--- tests/level_2/test_four_sentiment.py
+++ tests/level_2/test_four_sentiment.py
@@ -6,10 +6,14 @@ from functions.level_2.four_sentiment import check_tweet_sentiment
 @pytest.mark.parametrize(
     'text,expected',
     [
-        ('dislike slow bugs frustration', 'BAD'),
-        ('love caring', 'GOOD'),
-        ('Coming Home Learn more', None),
+        pytest.param('dislike slow bugs frustration', 'BAD', id='negative'),
+        pytest.param('love caring', 'GOOD', id='positive'),
+        pytest.param('Coming Home Learn more', None, id='neutral'),
     ],
 )
 def test__check_tweet_sentiment(text, good_words, bad_words, expected):
-    assert check_tweet_sentiment(text, good_words, bad_words) == expected
+    assert check_tweet_sentiment(
+        text=text,
+        good_words=good_words,
+        bad_words=bad_words
+    ) == expected
