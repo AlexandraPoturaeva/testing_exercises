1f4ffa30ee0040dbab2b5c9daafa7b19d764aa4e
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-09-20 12:13:49 +0300

    some small fixes

--- tests/level_2/test_five_replace_word.py
+++ tests/level_2/test_five_replace_word.py
@@ -4,31 +4,31 @@ from functions.level_2.five_replace_word import replace_word
 
 
 @pytest.mark.parametrize(
-    'text, expected',
+    'text,expected,id',
     [
-        pytest.param(
+        (
             'welcome to the jungle',
             'welcome to the forest',
-            id='replace_one_word',
+            'replace_one_word',
         ),
-        pytest.param(
+        (
             'welcome to the jungle we got your disease in the jungle',
             'welcome to the forest we got your disease in the forest',
-            id='replace_two_words',
+            'replace_two_words',
         ),
-        pytest.param(
+        (
             'welcome to the jungle. we got your disease in the jungle',
             'welcome to the jungle. we got your disease in the forest',
-            id='doesnt_work_with_punctuation'
+            'doesnt_work_with_punctuation',
         ),
-        pytest.param(
+        (
             'Welcome to the JunGle we got your disease in the jungle',
             'Welcome to the forest we got your disease in the forest',
-            id='independent_from_letter_case'
+            'independent_from_letter_case',
         ),
     ]
 )
-def test__replace_word(text, replace_from, replace_to, expected):
+def test__replace_word(text, replace_from, replace_to, expected, id):
     assert replace_word(text=text, replace_from=replace_from, replace_to=replace_to) == expected
 
 
--- tests/level_2/test_four_sentiment.py
+++ tests/level_2/test_four_sentiment.py
@@ -4,14 +4,14 @@ from functions.level_2.four_sentiment import check_tweet_sentiment
 
 
 @pytest.mark.parametrize(
-    'text,expected',
+    'text,expected,id',
     [
-        pytest.param('dislike slow bugs frustration', 'BAD', id='negative'),
-        pytest.param('love caring', 'GOOD', id='positive'),
-        pytest.param('Coming Home Learn more', None, id='neutral'),
+        ('dislike slow bugs frustration', 'BAD', 'negative'),
+        ('love caring', 'GOOD', 'positive'),
+        ('Coming Home Learn more', None, 'neutral'),
     ],
 )
-def test__check_tweet_sentiment(text, good_words, bad_words, expected):
+def test__check_tweet_sentiment(text, good_words, bad_words, expected, id):
     assert check_tweet_sentiment(
         text=text,
         good_words=good_words,
