1a420602c4fd53380e8fdabfa6ea725954deaf71
AlexandraPoturaeva <av-poturaeva@yandex.ru>
2023-10-10 13:34:18 +0300

    ex.4: add more tests

--- tests/level_3/test_four_fraud.py
+++ tests/level_3/test_four_fraud.py
@@ -7,3 +7,15 @@ def test__find_fraud_expenses(create_expense, create_random_bankcard):
     not_frauds = [create_expense(amount=Decimal(5000.01), card=create_random_bankcard()) for _ in range(3)]
 
     assert find_fraud_expenses(history=frauds + not_frauds) == frauds
+
+
+def test__find_fraud_expenses__no_frauds(create_expense, create_random_bankcard):
+    expenses = [create_expense(amount=Decimal(5000.01), card=create_random_bankcard()) for _ in range(3)]
+    assert not find_fraud_expenses(history=expenses)
+
+
+def test__find_fraud_expenses__not_enough_frauds(create_expense, create_random_bankcard):
+    frauds = [create_expense(card=create_random_bankcard()) for _ in range(2)]
+    not_frauds = [create_expense(amount=Decimal(5000.01), card=create_random_bankcard()) for _ in range(3)]
+
+    assert not find_fraud_expenses(history=frauds + not_frauds)
